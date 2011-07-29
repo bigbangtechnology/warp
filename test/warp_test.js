37dfa77226be5d2e6be9472e526ce2f324f56f78
Cameron Westland <cameron@bigbangtechnology.com>
2011-07-28 21:25:20 -0400

    Extracted warp into a class

--- test/warp_test.js
+++ test/warp_test.js
@@ -1,23 +1,12 @@
-var jsdom = require('jsdom'),
-fs = require('fs');
+var Warp = require('../lib/warp.js');
 
 var jQueryPath = 'http://code.jquery.com/jquery-1.4.2.min.js';
 
 exports["When you perform a GET you should have the page accessible after loaded"] = function(test) {
-  sut = {
-    visit: function(options) {
-      jsdom.env({
-        html: "<p>Hello World!</p>",
-        scripts: [ jQueryPath ],
-        done: function(errors, window) {
-          options.loaded(window.$);
-        }
-      });
-    }
-  }
+  sut = new Warp();
 
   sut.visit({
-    host: 'http://example.com',
+    host: 'example.com',
     path: '/',
     loaded: function($) {
       test.equal($('p').html(), "Hello World!", "The body should have a paragraph with Hello World in it");
@@ -25,3 +14,5 @@ exports["When you perform a GET you should have the page accessible after loaded
     }
   });
 }
+
+
