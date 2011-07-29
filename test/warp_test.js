4f9cf9b282046a032d89162f0505c85fb8d29e88
Cameron Westland <cameron@bigbangtechnology.com>
2011-07-28 21:14:48 -0400

    Loaded DOM via jsdom after getting a page.

--- test/warp_test.js
+++ test/warp_test.js
@@ -1,22 +1,25 @@
+var jsdom = require('jsdom'),
+fs = require('fs');
+
+var jQueryPath = 'http://code.jquery.com/jquery-1.4.2.min.js';
 
 exports["When you perform a GET you should have the page accessible after loaded"] = function(test) {
   sut = {
     visit: function(options) {
-      options.loaded(function() {
-        return {
-          html: function() {
-            return "Hello World!";
-          }
-
+      jsdom.env({
+        html: "<p>Hello World!</p>",
+        scripts: [ jQueryPath ],
+        done: function(errors, window) {
+          options.loaded(window.$);
         }
-      }, null);
+      });
     }
   }
 
   sut.visit({
     host: 'http://example.com',
     path: '/',
-    loaded: function($, html) {
+    loaded: function($) {
       test.equal($('p').html(), "Hello World!", "The body should have a paragraph with Hello World in it");
       test.done();
     }
